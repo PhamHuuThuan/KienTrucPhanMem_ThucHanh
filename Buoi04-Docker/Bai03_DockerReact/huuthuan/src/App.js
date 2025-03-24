6f531f14247ce9085649d9b40a162f1b83acb717
PhamHuuThuan <92664025+PhamHuuThuan@users.noreply.github.com>
2025-03-24 22:52:41 +0700

    Bai03 DockerReact

---
+++ Buoi04-Docker/Bai03_DockerReact/huuthuan/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '24px',
+      fontWeight: 'bold'
+    }}>
+      21004295 - Pham Huu Thuan
+    </div>
+  );
+}
+
+export default App;
